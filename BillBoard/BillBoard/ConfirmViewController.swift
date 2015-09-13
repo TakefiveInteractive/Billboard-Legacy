a1277b7648f83cd46464b349ebb0b02f1f96a5fd
likedan <likedan5@icloud.com>
2015-09-13 12:20:23 -0500

    mock version

--- BillBoard/BillBoard/ConfirmViewController.swift
+++ BillBoard/BillBoard/ConfirmViewController.swift
@@ -15,18 +15,33 @@ class ConfirmViewController: UIViewController, UITableViewDataSource, UITableVie
     @IBOutlet weak var cancel: UIButton!
     @IBOutlet weak var billout: UIButton!
     
-    let data = [""]
-    
+    let nameData = ["Yifei Teng","Ruth Cole","Angela Carter"]
+    let amountData = ["$ 23.23","$ 396.00","$ 75.00"]
+
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
+        return nameData.count
     }
     
+    @IBAction func billOutConfirm(sender: AnyObject) {
+        UIApplication.sharedApplication().openURL(NSURL(string: "venmo://")!)
+    }
+
+    @IBAction func exit(sender: AnyObject) {
+        self.view.removeFromSuperview()
+        self.removeFromParentViewController()
+    }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
+        var cell: AnyObject? = tableView.dequeueReusableCellWithIdentifier("confirmcell")
+        (cell as! UITableViewCell).selectionStyle = UITableViewCellSelectionStyle.None
+        (cell as! ConfirmCell).peopleName.text = nameData[indexPath.item]
+        (cell as! ConfirmCell).amount.text = amountData[indexPath.item]
+        (cell as! ConfirmCell).peopleIcon.image = MockDataManager.randomAvatar()
+        return cell as! UITableViewCell
     }
     
     override func viewDidLoad() {
-        
+        table.delegate = self
+        table.dataSource = self
     }
     
 }
@@ -35,6 +50,6 @@ class ConfirmViewController: UIViewController, UITableViewDataSource, UITableVie
 class ConfirmCell: UITableViewCell {
     @IBOutlet weak var peopleIcon: UIImageView!
     @IBOutlet weak var amount: UILabel!
-    @IBOutlet weak var peopleNum: UILabel!
+    @IBOutlet weak var peopleName: UILabel!
     
 }
